f6d11785a0bf3ef1e310e25b16b298873cc7e965
Dariusz Okonski <darek200180@gmail.com>
2021-06-27 14:18:07 +0200

    make hero to run over inventory items without collection

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -6,11 +6,13 @@ import com.codecool.dungeoncrawl.logic.Drawable;
 
 public abstract class Actor implements Drawable {
     private Cell cell;
+    private Actor inventoryItem;
     private int health = 10;
 
     public Actor(Cell cell) {
         this.cell = cell;
         this.cell.setActor(this);
+        this.inventoryItem = null;
     }
 
     public void move(int dx, int dy) {
@@ -29,22 +31,47 @@ public abstract class Actor implements Drawable {
 //        System.out.println(nextCell.getActor() instanceof Sword);
 //        System.out.println(nextCell.getActor() instanceof Key);
 
+        // if hit the wall
         if(nextCell.getTileName() == CellType.WALL.getTileName())
             return;
 
+        // if hero step off item without collecting
         if((nextCell.getTileName() == CellType.FLOOR.getTileName())
-                &&((nextCell.getActor() == null)
-                || (nextCell.getActor() instanceof Sword)
-                || (nextCell.getActor() instanceof Key)))
-        {
-            //TODO - add to inventory
-            addToInventory(nextCell);
+                &&(nextCell.getActor() == null) && (inventoryItem != null)) {
 
+            if(inventoryItem instanceof Sword)
+                cell.setActor(new Sword(cell));
+
+            if(inventoryItem instanceof Key)
+                cell.setActor(new Key(cell));
+
+            nextCell.setActor(this);
+            cell = nextCell;
+            inventoryItem = null;
+            return;
+        }
+
+        // moving around the board on floor
+        if((nextCell.getTileName() == CellType.FLOOR.getTileName())
+                &&(nextCell.getActor() == null))
+        {
             cell.setActor(null);
             nextCell.setActor(this);
             cell = nextCell;
             return;
         }
+
+        // collect an item logic
+        if((nextCell.getActor() instanceof Sword) || (nextCell.getActor() instanceof Key)) {
+            System.out.println("Collect item?");
+
+            inventoryItem = nextCell.getActor();
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+            return;
+        }
+
 
         //TODO - fight with enemy or collect some items
 //        System.out.println("============> fight with enemy or collect some items");
